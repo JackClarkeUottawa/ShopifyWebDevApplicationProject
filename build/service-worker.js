74c2b95ac0be032291e1ef6727aa7b87f4b0c32c
Jack Clarke <jclar162@uottawa.ca>
2020-09-09 17:54:30 -0400

    reverted to last sane commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ShopifyWebDevApplicationProject/precache-manifest.929bd738ad4d44e7a79ad07884ad9c39.js"
+  "/precache-manifest.d960f4ac34321fc903d1822fe903d72b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShopifyWebDevApplicationProject/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
